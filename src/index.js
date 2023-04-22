85b4529309fc2089b0efe4fecaa2d5e911c7637d
AlienX <aditya1115.ar@gmail.com>
2023-04-22 22:54:00 +0530

    final

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import './index.css';
 ReactDOM.render(
   <Auth0Provider
     // fetch from .env file
-    domain={process.env.DOMAIN}
-    clientId={process.env.CLIENT_ID}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
